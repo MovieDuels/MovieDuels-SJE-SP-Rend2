a1fb029cd96f0a1ba8d375544ac379ad0cd0d097
jacesolaris <Jacesolaris@hotmail.com>
2026-07-13 14:48:44 +0100

    Swing Blocking is now Blockpoint based. 75 BP or more allows swing blocking.

--- code/game/bg_pmove.cpp
+++ code/game/bg_pmove.cpp
@@ -19605,6 +19605,12 @@ static void PM_WeaponLightsaber(void)
 		}
 
 		pm->ps->weaponTime -= pml.msec;
+
+		if (is_holding_block_button)
+		{ // keep him in the blocking pose until he can attack again
+			PM_SetAnim(pm, SETANIM_FLAG_NORMAL, saberMoveData[pm->ps->saberMove].animToUse, saberMoveData[pm->ps->saberMove].animSetFlags | SETANIM_FLAG_HOLD);
+			return;
+		}
 	}
 	else
 	{
@@ -20858,43 +20864,46 @@ static void PM_WeaponLightsaber(void)
 	// We are in a firing state for the weapon.
 	pm->ps->weaponstate = WEAPON_FIRING;
 
-	// If we are still in weaponTime and holding block, force a blocking pose.
-	if (pm->ps->weaponTime > 0 && (is_holding_block_button))
+	// If we are still in weaponTime and holding block, with enough BP force a blocking pose.
+	if (pm->ps->blockPoints > BLOCKPOINTS_MISSILE)
 	{
-		if (pm->ps->saberAnimLevel == SS_STAFF)
+		if (pm->ps->weaponTime > 0 && (is_holding_block_button))
 		{
-			if (g_SerenityJediEngineMode->integer == 2)
+			if (pm->ps->saberAnimLevel == SS_STAFF)
 			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelStaffAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				if (g_SerenityJediEngineMode->integer == 2)
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelStaffAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
+				else
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelStaffMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
+			}
+			else if (pm->ps->saberAnimLevel == SS_DUAL)
+			{
+				if (g_SerenityJediEngineMode->integer == 2)
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelDualAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
+				else
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelDualMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
 			}
 			else
 			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelStaffMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				if (g_SerenityJediEngineMode->integer == 2)
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelSingleAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
+				else
+				{
+					PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelSingleMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
+				}
 			}
+			PM_SetSaberMove(LS_READY);
 		}
-		else if (pm->ps->saberAnimLevel == SS_DUAL)
-		{
-			if (g_SerenityJediEngineMode->integer == 2)
-			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelDualAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
-			}
-			else
-			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelDualMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
-			}
-		}
-		else
-		{
-			if (g_SerenityJediEngineMode->integer == 2)
-			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelSingleAMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
-			}
-			else
-			{
-				PM_SetAnim(pm, SETANIM_TORSO, PM_BlockingPoseForSaberAnimLevelSingleMD(), SETANIM_FLAG_OVERRIDE | SETANIM_FLAG_HOLD);
-			}
-		}
-		PM_SetSaberMove(LS_READY);
 	}
 
 	// If this entity has a fireDelay, we are not actually firing yet.
